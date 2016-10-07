ab469faaa5b8d38c1cf74609cafa80ad6b95c57c
Milton Moura <milton@amphetamobile.com>
2016-10-07 21:17:46 +0000

    added support for recursive decoding

--- unobfuscate.php
+++ unobfuscate.php
@@ -1,15 +1,27 @@
 <?php
 
+function lame_decode($content) {
+  $instructions = explode(";", $content);
+  $step_3 = str_replace("eval(", "", $instructions[3]);
+  $step_3 = str_replace("))", ");", $step_3);
+  $step_3 = str_replace("\$_D", "return base64_decode", $step_3);
+  $result = eval($step_3);
+  $result = str_replace("\$_R=str_replace('__FILE__',\"'\".\$_F.\"'\",\$_X);eval(\$_R);\$_R=0;\$_X=0;", "return \$_X;", $result);
+  $result = "$instructions[1];$result";
+  return $result;
+}
+
 $filename = $argv[1];
 $file_content = file_get_contents($filename);
-$instructions = explode(";", $file_content);
+$result = lame_decode($file_content);
+$code = eval($result);
 
-$step_3 = str_replace("eval(", "", $instructions[3]);
-$step_3 = str_replace("))", ");", $step_3);
-$step_3 = str_replace("\$_D", "return base64_decode", $step_3);
-$result = eval($step_3);
-$result = str_replace("\$_R=str_replace('__FILE__',\"'\".\$_F.\"'\",\$_X);eval(\$_R);\$_R=0;\$_X=0;", "return \$_X;", $result);
-$result = "$instructions[1];$result";
-echo eval($result);
+// support for recursive encoding
+while (strpos($code, "edoced_46esab") !== false) {
+  $result = lame_decode($code);
+  $code = eval($result);
+}
+
+echo "$code\n";
 
 ?>
